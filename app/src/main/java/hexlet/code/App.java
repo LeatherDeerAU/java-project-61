6ac436234a6f67b2eb0b5ba9fa487ea86c914aa3
LeatherDeerAU <mJ23@23joqw>
2023-03-04 19:24:28 +0300

    added check even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
-        CLI.run();
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Check
+                0 - Exit
+                """.trim());
+
+        System.out.print("Your choice: ");
+        int userChoise = scanner.nextInt();
+
+        System.out.println();
+        if (userChoise == 2) {
+            EvenCheckGame.run();
+        } else if (userChoise == 1) {
+            CLI.runGreeting();
+        } else if (userChoise == 0) {
+            System.out.println("Exit.");
+        }
     }
 }
--- app/src/main/java/hexlet/code/CLI.java
+++ app/src/main/java/hexlet/code/CLI.java
@@ -3,8 +3,9 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class CLI {
-    public static void run() {
+    public static void runGreeting() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
         String name = scanner.nextLine();
